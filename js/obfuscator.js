0a629a6fec4360041e7298e31bc0f51aa0956d7b
WEINI <opaajhone@gmail.com>
2026-01-31 01:20:27 +0800

    更新 obfuscator.js

--- js/obfuscator.js
+++ js/obfuscator.js
@@ -1,5 +1,12 @@
 function renameLocals(code){
-    return code.replace(/\blocal\s+([a-zA-Z_]\w*)/g,()=>{
-        return "local _"+Math.random().toString(36).slice(2,10);
+    const map = {};
+
+    return code.replace(/\blocal\s+([a-zA-Z_]\w*)\s*=\s*(?!function)/g, (m, name) => {
+        if(!map[name]){
+            map[name] = "_" + Math.random().toString(36).slice(2,10);
+        }
+        return "local " + map[name] + " =";
+    }).replace(/\b([a-zA-Z_]\w*)\b/g, (m, name) => {
+        return map[name] || name;
     });
 }
